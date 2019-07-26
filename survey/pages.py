e054691c5eeb2553f990ab10ca3eb1e7bb13f821
Danlin Chen <d84chen@uwaterloo.com>
2019-07-26 22:45:18 +0800

    out

--- survey/pages.py
+++ survey/pages.py
@@ -31,10 +31,8 @@ class Results(Page):
         if self.session.config['Ireland']:
             self.player.link = '5;URL=https://cessonline.eu.qualtrics.com/jfe/form/SV_eYhSf4Ze2fVGE17?expost_ireland=1&participant_label=' + str(self.player.participant.label)
         if self.session.config['Netherland']:
-            if 18 <= self.player.age <= 65 and self.player.gender in ['man', 'vrouw']:
-                self.player.link = '5;URL=https://mingle.respondi.com/s/1142257/ospe.php3?c_0002=1&return_tic='+str(self.player.participant.label)
-            else:
-                self.player.link = '5;URL=https://mingle.respondi.com/s/1142257/ospe.php3?c_0002=0&return_tic='+str(self.player.participant.label)
+            self.player.link = '5;URL=https://mingle.respondi.com/s/1142257/ospe.php3?c_0002=1&return_tic='+str(self.player.participant.label)
+
         return {
             'p1': p1,
             'p2': p2,
@@ -58,6 +56,9 @@ class Comments(Page):
 class Thankyou(Page):
     def is_displayed(self):
         return self.player.participant.vars['consent'] == False
+
+    def vars_for_template(self):
+        self.player.link = '5;URL=https://mingle.respondi.com/s/1142257/ospe.php3?c_0002=0&return_tic=' + str(self.player.participant.label)
 
 page_sequence = [
     demographic,
